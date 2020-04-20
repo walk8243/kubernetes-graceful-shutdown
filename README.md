8f5a6316dfc964557ae8e03b34b990126b1867b9
walk8243 <walk8243@gmail.com>
2020-04-21 00:01:47 +0900

    定期的にslackにメッセージを送信する

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # kubernetes-graceful-shutdown
+
 KubernetesのGracefulShutdownのときの動作を確認する
+
+## Kubernetesへのデプロイ
+
+### ConfigMap
+
+```.sh
+kubectl apply -f configmap.yaml
+```
+
+### Secret
+
+```.sh
+echo -n '__SLACKのメッセージ送信用のトークン__' > token
+kubectl create secret generic slack-auth --from-file=./token
+```
+
+### Pod
+
+```.sh
+kubectl apply -f pod.yaml
+```
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const https = require('https');
+
 writeLog();
 setInterval(() => {
 	writeLog();
@@ -6,4 +8,24 @@ setInterval(() => {
 function writeLog() {
 	const date = new Date();
 	console.log('Hello World!', date.toLocaleString());
+	sendSlack(`[${date.toLocaleString()}] Regular message.`);
+}
+
+function sendSlack(text) {
+	const req = https.request('https://slack.com/api/chat.postMessage', {
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${process.env['SLACK_TOKEN']}`,
+			'Content-Type': 'application/json; charset=UTF-8',
+		}
+	}, (res) => {
+		res.setEncoding('utf8');
+		res.on('data', (chunk) => {
+			console.log(chunk);
+		});
+		res.on('end', () => {});
+	});
+
+	req.write(JSON.stringify({ channel: 'post-message', text }));
+	req.end();
 }
